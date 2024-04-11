ca2828d0f2cdd52bea113e46b48999aef0e65613
qkrwnsdn981204 <qkrwnsdn981204@gmail.com>
2024-04-11 15:30:03 +0900

    기본 설정

---
+++ src/main/java/org/spring/e1i4TeamProject/E1i4TeamProjectApplication.java
@@ -0,0 +1,13 @@
+package org.spring.e1i4TeamProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E1i4TeamProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(E1i4TeamProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/spring/e1i4TeamProject/E1i4TeamProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spring.e1i4TeamProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class E1i4TeamProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
